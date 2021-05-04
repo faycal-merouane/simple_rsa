4b7689c37af019588ee1d402642cc6a6fe464c57
faycal-merouane <53337523+faycal-merouane@users.noreply.github.com>
2021-05-04 12:16:07 +0100

    Update SwiftSimpleRsaPlugin.swift

--- ios/Classes/SwiftSimpleRsaPlugin.swift
+++ ios/Classes/SwiftSimpleRsaPlugin.swift
@@ -39,7 +39,7 @@ public class SwiftSimpleRsaPlugin: NSObject, FlutterPlugin {
 			case "decryptWithPublicKey":
 				let text : String = argsMap["plainText"] as! String
 				let publicKey : String = argsMap["publicKey"] as! String			
-				let res = decryptString(text,publicKey)
+				let res = RSAUtil.decryptString(text,publicKey)
     				result(res)
 			default:
 				result(FlutterMethodNotImplemented)
